bdb0e8eefe1475baf1204e5a77cfc2118533f521
arthurb123 <arthieb@live.nl>
2018-12-24 22:45:10 +0100

    Chat optimalizations

--- client/lib/ui.js
+++ client/lib/ui.js
@@ -15,7 +15,7 @@ const ui = {
             
             view.dom.innerHTML += 
                 '<div id="chat_box" class="box" style="position: absolute; top: 100%; left: 35px; margin-top: -235px; width: 340px; height: 180px;">' +
-                    '<div id="chat_box_content" class="content" style="height: 155px;"></div>' +
+                    '<div id="chat_box_content" class="content" style="overflow-y: auto; height: 155px;"></div>' +
                     '<input id="chat_box_message" type="text" style="width: 262px;"></textbox>' +
                     '<button onclick="ui.chat.sendMessage()" style="position: relative; left: 2px; height: 20px; width: 70px; padding-top: 2px;">Send</button>' +
                 '</div>';
@@ -34,11 +34,19 @@ const ui = {
         timeformat: function() {
             return '[' + new Date().toString().substr(16, 5) + '] ';
         },
+        clear: function() {
+            if (this.dom === undefined)
+                return;
+            
+            this.dom.content.innerHTML = '';
+        },
         addMessage: function(content) {   
             if (this.dom === undefined)
                 return;
             
-            this.dom.content.innerHTML += '<font class="info">' + this.timeformat() + content + '</font><br>';
+            this.dom.content.innerHTML += '<font style="display: inline;" class="info">' + this.timeformat() + content + '</font><br>';
+            
+            this.dom.content.scrollTo(0, this.dom.content.scrollHeight);
         },
         sendMessage: function() {
             if (this.dom === undefined ||
