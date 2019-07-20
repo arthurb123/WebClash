ced66c8665e2aaf814524b9781542968623bff01
Arthur <arthieb@live.nl>
2019-07-20 23:29:27 +0200

    Removed testing map, fixed wording in client properties

--- client/properties.js
+++ client/properties.js
@@ -25,9 +25,9 @@ const properties = {
     darknessColor: "black",
     darknessOpacity: 0.45,
 
-    //The standard rendering scale,
-    //higher is a zoom-in, lower
-    //is a zoom-out.
+    //The rendering scale, higher 
+    //is a zoom-in, lower is a 
+    //zoom-out.
 
     computerRenderScale: 2,
     mobileRenderScale: 1,
